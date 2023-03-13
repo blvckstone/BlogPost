8e8ba67baee6611ff62ddcb89ac7c48a099583ef
Blvckstone <mohammadshoiab9011@gmail.com>
2023-03-13 23:51:54 +0530

    Used express route parameter for avoid multiple pages

--- app.js
+++ app.js
@@ -1,58 +1,77 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 const ejs = require("ejs");
 
 let postArray = [];
 // let titleArray = [];
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent =
+  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent =
+  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent =
+  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+app.set("view engine", "ejs");
 
-
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
-  res.render("home", {homeShow: homeStartingContent});
-})
-
-app.get("/about", function(req, res){
-  res.render("about", {showAbout: aboutContent});
-})
 
 
-app.get("/contact", function(req, res){
-  res.render("contact", {showContact: contactContent});
-})
+app.get("/", function (req, res) {
+  res.render("home", { homeShow: homeStartingContent, postArray: postArray });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", { showAbout: aboutContent });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { showContact: contactContent });
+});
 
 
-app.post("/compose", function(req, res){
-    // console.log(req.body.message)
 
-    let post = {
+app.get("/posts/:topic", function(req, res){
+  let topic = (req.params.topic);
+  
+  
 
-      title: req.body.title,
-      message: req.body.message,
 
+  console.log(topic)
+
+  postArray.forEach(function(arrayItems){
+    console.log(arrayItems.title)
+    if(arrayItems.title == topic){
+
+      console.log("Match Found");
+    }else {
+      console.log("Match Not Found")
     }
-
-    postArray.push(post)
-    
-
-
-    res.redirect("/compose")
-
-
+  })
 })
 
-app.get("/compose", function(req, res){
 
+
+
+
+app.post("/compose", function (req, res) {
+  // console.log(req.body.message)
+
+  let post = {
+    title: req.body.title,
+    message: req.body.message,
+  };
+
+  postArray.push(post);
+
+  res.redirect("/");
+});
+
+app.get("/compose", function (req, res) {
   // let arrayElement = '';
 
   // for(let i=0; i<messageArray.length; i++){
@@ -61,23 +80,9 @@ app.get("/compose", function(req, res){
 
   // }
 
+  res.render("compose", { postArray: postArray });
+});
 
-  res.render("compose", {postArray: postArray})
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
